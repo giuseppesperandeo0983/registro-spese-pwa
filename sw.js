c8cab476582958b9198baf92f57f97fa4992c0fa
giuseppesperandeo0983 <giuseppe.sperandeo09@gmail.com>
2025-12-18 01:03:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,16 @@
-const CACHE_NAME = "registro-spese-cache-v1";
-const urlsToCache = [
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-];
+const CACHE_NAME = "registro-spese-v1";
+const urlsToCache = ["./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", event=>{
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", event=>{
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response=>{
+      return response || fetch(event.request);
+    })
   );
 });
